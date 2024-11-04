942a016a88b09c371829dfe8839e104397dd335b
Chuck Houpt <chuck@habilis.net>
2024-11-04 09:06:03 -0500

    Update automatic bundle-version for git
    - Add git description for tracking hash and tags
    - Correct build-phase deps so runs after Info.plist processing
    - Simplify script to only emit warnings on failure

--- Set-CFBundleVersion.sh
+++ Set-CFBundleVersion.sh
@@ -2,49 +2,40 @@
 
 #  Set-CFBundleVersion.sh
 #
-#  Sets the built-product's CFBundeVersion to the current working-directory Subversion revision.
+#  Sets the built-product's CFBundeVersion to the current working-directory revision.
 #
-#  Install by adding a Run-Script Phase to the end of target's Build Phases with the script:
+#  Install by adding a Run-Script Phase to the end of target's Build Phases with:
 #
-#       $SRCROOT/Set-CFBundleVersion.sh
+#    Script:  $SRCROOT/Set-CFBundleVersion.sh
+#    Based on dependency analysis: False
+#    Input Files: $(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)
 #
 #  Select "Run script only when installing" to limit execution to Release/Archiving.
 #
 #  Created by Chuck Houpt on 10/23/13.
-#  Copyright 2013 Behavioral Cybernetics LLC. All rights reserved.
+#  Copyright 2013-2024 Behavioral Cybernetics LLC. All rights reserved.
 
-set -eu
-
-exit
+set -eux
 
 # Only run on release builds, since debug builds are by definition based on un-commmited code changes.
-if [ $CONFIGURATION != 'Release' ] ; then exit ; fi
+#if [ $CONFIGURATION != 'Release' ] ; then exit ; fi
 
-# Preemptively call svn-cleanup to avoid sqlite-errors from svnversion
-svn cleanup
-
-REVISION=$(svnversion -n)
-
-# Report problems if the working directory isn't a completely clean single-revision checkout.
-# Use REPORT var to switch between warning and error ("Jerk Mode")
-REPORT=warning
-# REPORT=error
-
-if [[ ! "$REVISION" =~ ^[0-9]+$ ]]
+if [[ ! -d .git ]]
 then
-        echo "$REPORT: working directory is not a single unmodified revision. svnversion reports: $REVISION (note: X:Y means mixed-revision, so update work-dir. 'M' means modified, so commit work-dir changes)"
-        [ $REPORT == 'warning' ]
-elif [[ $(svn status) ]]
-then
-        echo "warning: working directory is not a completely clean checkout with no extranious files. svn status reports:"
-        svn status
+    echo "warning: working dir not under version control, could not set version"
+    exit
 fi
+
+REVISION=$(git rev-list --count HEAD)
+DESCRIBE=$(git describe --tags --long --always --dirty=-mutant)
+
+# Emulate SVN by adding M if dirty
+case $DESCRIBE in
+  *-mutant) REVISION="${REVISION}M" ;;
+esac
 
 # Modify CFBundleVersion in the product's compiled Info.plist
 
-if grep --quiet 'CFBundleVersion' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"
-then
-        /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $REVISION" "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"
-else
-        /usr/libexec/PlistBuddy -c "Add :CFBundleVersion string $REVISION" "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"
-fi
+/usr/libexec/PlistBuddy -c "Add :CFBundleVersion string $REVISION" \
+    -c "Add :BCGitDescribe string $DESCRIBE" \
+    "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH" || echo "warning: failed to setup CFBundleVersion"
